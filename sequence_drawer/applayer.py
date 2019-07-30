400ed0a850c7d731e6adddc3c7bd85b886a52314
Allend95 <allen.dumler@fu-berlin.de>
2019-07-30 14:19:00 +0200

    cleaned up with black

--- sequence_drawer/applayer.py
+++ sequence_drawer/applayer.py
@@ -4,27 +4,29 @@ from PIL import ImageTk, Image
 
 
 def commandparser():
-	parser = argparse.ArgumentParser(description='Parse 2D View')
-	parser.add_argument('smile')
-	args = parser.parse_args()
-	smiles=args.smile
-	return smiles
+    parser = argparse.ArgumentParser(description="Parse 2D View")
+    parser.add_argument("smile")
+    args = parser.parse_args()
+    smiles = args.smile
+    return smiles
+
 
 def display():
-	window=tk.Tk()
-	window.title("Your molecule")
-	window.geometry("900x900")
-	window.configure(background="white")
-	windowimg = Image.open('./data/pictureofmolecule.png')
-	ph = ImageTk.PhotoImage(windowimg)
-	panel = tk.Label(window,image=ph)
-	panel.image = ph
-	panel.pack(side="bottom", fill="both",expand="yes")
-	window.mainloop()
+    window = tk.Tk()
+    window.title("Your molecule")
+    window.geometry("900x900")
+    window.configure(background="white")
+    windowimg = Image.open("./data/pictureofmolecule.png")
+    ph = ImageTk.PhotoImage(windowimg)
+    panel = tk.Label(window, image=ph)
+    panel.image = ph
+    panel.pack(side="bottom", fill="both", expand="yes")
+    window.mainloop()
 
-if __name__ == '__main__':
-	smiles = commandparser()
-	import drawer
-	drawer.convert_SMILES_to_Image(smiles)
-	display()
 
+if __name__ == "__main__":
+    smiles = commandparser()
+    import drawer
+
+    drawer.convert_SMILES_to_Image(smiles)
+    display()
--- sequence_drawer/tests/test_sequence_drawer.py
+++ sequence_drawer/tests/test_sequence_drawer.py
@@ -7,6 +7,7 @@ import sequence_drawer
 import pytest
 import sys
 
+
 def test_sequence_drawer_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "sequence_drawer" in sys.modules
